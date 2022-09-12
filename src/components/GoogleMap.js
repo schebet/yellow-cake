ea59f095d2bb9ad9e76496bcc1ca8815372e09ea
schebet <91207140+schebet@users.noreply.github.com>
2022-09-12 14:55:12 +0200

    Update GoogleMap.js

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -10,8 +10,8 @@ if (process.env.GATSBY_NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: 43.14248722502801,
+      lng: 22.172866053861462
     },
     zoom: 14
   }
